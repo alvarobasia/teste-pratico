7615735ead06bb593a0a3592302077bd85c77e6b
Álvaro Araújo <alvarobdaraujo@outlook.com>
2025-04-23 13:13:36 -0300

    Refactor employee filtering to use Fuse.js for fuzzy search
    Replaced manual string matching logic with Fuse.js to improve text search functionality for employee name and CPF. This change enhances the filtering precision and supports partial matches with configurable thresholds. Results from name and CPF searches are now combined intelligently to avoid duplicates.
    
    Signed-off-by: Álvaro Araújo <alvarobdaraujo@outlook.com>

--- src/utils/filter-employee.ts
+++ src/utils/filter-employee.ts
@@ -1,18 +1,44 @@
 import { Employee } from "@/types/employee.ts";
+import fuse from "fuse.js";
 
 export const filterEmployee = (
   name: string,
   cpf: string,
   employee: Employee[],
 ) => {
-  if (name === "" && cpf === "") {
+  let resultName: Employee[] = employee;
+  let resultCpf: Employee[] = employee;
+  if (name !== "") {
+    const fuseSearchName = new fuse(employee, {
+      keys: ["name"],
+      threshold: 0.3,
+      includeScore: true,
+    });
+    resultName = fuseSearchName.search(name).map((result) => result.item);
+  }
+  if (cpf !== "") {
+    const fuseSearchCpf = new fuse(employee, {
+      keys: ["cpf"],
+      threshold: 0.3,
+      includeScore: true,
+    });
+    resultCpf = fuseSearchCpf.search(cpf).map((result) => result.item);
+  }
+  const isNotFilteredByName = employee.length === resultName.length;
+  const isNotFilteredByCpf = employee.length === resultCpf.length;
+
+  if (isNotFilteredByName && isNotFilteredByCpf) {
     return employee;
   }
-  const filteredEmployees = employee.filter((emp) => {
-    const nameMatch = emp.name.toLowerCase().includes(name.toLowerCase());
-    const cpfMatch = emp.cpf.includes(cpf);
-    return nameMatch || cpfMatch;
-  });
-
-  return filteredEmployees;
+  if (isNotFilteredByName && !isNotFilteredByCpf) {
+    return resultCpf;
+  }
+  if (isNotFilteredByCpf && !isNotFilteredByName) {
+    return resultName;
+  }
+  const uniqueResults = new Set<Employee>([
+    ...resultName,
+    ...resultCpf,
+  ]).values();
+  return Array.from(uniqueResults) as Employee[];
 };
