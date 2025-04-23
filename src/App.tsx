adea90226abc4f64959750672e2eabece70a7085
Álvaro Araújo <alvarobdaraujo@outlook.com>
2025-04-23 13:13:47 -0300

    Adjust spacing in employee management header container
    Added a `my-3` class to the header container for better vertical spacing and improved layout appearance. This enhances the visual balance of the component.
    
    Signed-off-by: Álvaro Araújo <alvarobdaraujo@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import EmployeeManagement from "@/components/employee-management.tsx";
 import EmployeeTable from "@/components/employee-table.tsx";
+import EmployeeFilter from "@/components/employee-filter.tsx";
+import { useEffect } from "react";
+import { init } from "@/redux/employee-slice.ts";
+import { useDispatch } from "react-redux";
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(init());
+  }, [dispatch]);
+
   return (
     <>
+      <EmployeeFilter />
       <EmployeeManagement />
       <EmployeeTable />
     </>
