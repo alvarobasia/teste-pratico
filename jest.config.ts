1aa303d1f041c1fd9e8d747a2ef603f59cf1df09
Álvaro Araújo <alvarobdaraujo@outlook.com>
2025-04-23 23:08:17 -0300

    Add Jest configuration and setup for testing
    Introduce Jest configuration file with TypeScript support and jsdom as the test environment. Add a test setup file to initialize utilities like TextEncoder and TextDecoder globally.
    
    Signed-off-by: Álvaro Araújo <alvarobdaraujo@outlook.com>

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  testMatch: ["**/tests/**/*.spec.(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+};
+
+export default config;
